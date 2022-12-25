cfc5a489502065d656b339a9b5a1a7345a6d535a
praveenbhosle <praveen.bhosle5@gmail.com>
2022-12-25 21:29:25 +0530

    hashset demo added

---
+++ src/main/java/com/employeemanagement/controller/HashSetDemo.java
@@ -0,0 +1,17 @@
+package com.employeemanagement.controller;
+
+import java.util.HashSet;
+
+public class HashSetDemo {
+
+	public static void main(String[] args) {
+		HashSet<Integer> set = new HashSet<>();
+
+		set.add(100);
+		set.add(200);
+		set.add(300);
+		
+		System.out.println(set);
+	}
+
+}
