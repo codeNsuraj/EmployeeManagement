2341a3e6d75863c86cadf9ee2be1ee721b1e46fc
BhoslePraveen <111246105+BhoslePraveen@users.noreply.github.com>
2023-01-08 21:01:07 +0530

    Update TestDemo.java

--- src/main/java/com/employeemanagement/controller/TestDemo.java
+++ src/main/java/com/employeemanagement/controller/TestDemo.java
@@ -8,7 +8,6 @@ public class TestDemo {
 	private String mobileno;
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
-		System.out.println("review");
+		System.out.println("new code added");
 	}
 }
