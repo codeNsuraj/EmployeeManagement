bfc9ace9e5dad28d7eb33d747f081e68dd746d35
BhoslePraveen <111246105+BhoslePraveen@users.noreply.github.com>
2023-01-08 20:55:48 +0530

    Update TestDemo.java

--- src/main/java/com/employeemanagement/controller/TestDemo.java
+++ src/main/java/com/employeemanagement/controller/TestDemo.java
@@ -8,6 +8,7 @@ public class TestDemo {
 	private String mobileno;
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		System.out.println("master priniting from here");
+		System.out.println("manager code is addded here");
 	}
 }
