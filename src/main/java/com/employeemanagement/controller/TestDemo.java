1de38a48522ea472ee883502e5c79132b679827f
praveenbhosle <praveen.bhosle5@gmail.com>
2023-01-08 20:01:46 +0530

    Bug is fixed

--- src/main/java/com/employeemanagement/controller/TestDemo.java
+++ src/main/java/com/employeemanagement/controller/TestDemo.java
@@ -1,8 +1,11 @@
 package com.employeemanagement.controller;
 
 public class TestDemo {
-	
+
 	private int id;
 	private String name;
 
+	public static void main(String[] args) {
+		System.out.println("hello we have solved our bug");
+	}
 }
